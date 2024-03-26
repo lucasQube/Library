4da97a6e9a77940762668eb54fdee5183da6f8d1
lucas <lucas.pan@qubeapps.com>
2024-03-26 18:17:56 +0800

    Initial Commit

---
+++ src/LibraryServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Test\Library;
+
+use Illuminate\Support\ServiceProvider;
+
+Class LibraryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
---
+++ src/Services/PhpEncryption.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Test\Library\Services;
+
+Class PhpEncryption
+{
+    public function Encryption()
+    {
+        return "1234-abcd-5678";
+    }
+}
